a1b4acbee2df813cb7224ed0aa825b369ecdf776
Oleksii Sheludchenko <oleksii.sheludchenko@gmail.com>
2023-06-05 11:05:23 +0300

    Created agent controller project

---
+++ agent-controller/program.cs
@@ -0,0 +1,25 @@
+﻿using k8s;
+using k8s.Models;
+
+var pollInterval = Environment.GetEnvironmentVariable("POLL_INTERVAL") ?? "10000";
+var targetNamespaceName = Environment.GetEnvironmentVariable("TARGET_NAMESPACE") ?? "ado-agent";
+
+var kubernetesConfigurationPath = Environment.GetEnvironmentVariable("KUBECONFIG") ?? 
+                                  String.Format("{0}/.kube/config", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+
+var kubernetesConfiguration = KubernetesClientConfiguration.LoadKubeConfig(kubernetesConfigurationPath);
+var kubernetesClientConfiguration = KubernetesClientConfiguration.BuildConfigFromConfigFile(kubernetesConfigurationPath, kubernetesConfiguration.CurrentContext);
+
+var client = new Kubernetes(kubernetesClientConfiguration);
+
+var namespaces = client.CoreV1.ListNamespace();
+
+var targetNamespace = new V1Namespace {
+  Metadata = new V1ObjectMeta {
+    Name = targetNamespaceName
+  }
+};
+
+if (!namespaces.Items.Any(item => item.Metadata.Name == targetNamespaceName)) {
+  client.CoreV1.CreateNamespace(targetNamespace);
+}
